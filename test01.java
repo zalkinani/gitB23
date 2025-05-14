619e2305bd2f2edfe7259f2bf343c9fb08c9770b
zalkinani <zahraa.alkinani@gmail.com>
2025-05-14 18:58:12 -0500

    2nd commit

--- test01.java
+++ test01.java
@@ -3,5 +3,6 @@ package class02;
 public class test01 {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");System.out.println("hello");System.out.println("hello");System.out.println("hello");
     }
 }
